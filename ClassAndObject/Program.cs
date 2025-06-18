38aeb2362dd907c9a8560c620b0a9437f37c56f0
MUGHILAN_OFFICIAL <mca211117@smvec.ac.in>
2025-06-18 15:02:30 +0530

    Add project files.

---
+++ ClassAndObject/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassAndObject
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Prime myobj = new Prime();
+            myobj.primeNumber();
+
+            Factorial myobj2 = new Factorial();
+            myobj2.factorialNumber();   
+        }
+    }
+}
---
+++ ClassAndObject/primeNumber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    internal class Prime
+    {
+        public void primeNumber()
+        {
+                Console.Write("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                bool isPrime = true;
+
+                if (num <= 1)
+                {
+                    isPrime = false;
+                }
+                else
+                {
+                    for (int i = 2; i < num; i++)
+                    {
+                        if (num % i == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine($"Prime number: {num}");
+                }
+                else
+                {
+                    Console.WriteLine($"Not Prime Number: {num} ");
+                }
+        }
+
+
+
+    }
+
+}
